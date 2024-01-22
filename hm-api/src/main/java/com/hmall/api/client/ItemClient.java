f66af55b929a0feca81dec6587250ed97aaf8556
dy <dyu32345@gmail.com>
2024-01-22 21:25:59 +0800

    OpenFeign 最佳实践
    将 client 抽取到微服务之外的 module中

---
+++ hm-api/src/main/java/com/hmall/api/client/ItemClient.java
@@ -0,0 +1,20 @@
+package com.hmall.api.client;
+
+import com.hmall.api.dto.ItemDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * @Author: dy
+ * @Date: 2024/1/21 11:13
+ * @Description:
+ */
+@FeignClient("item-service")
+public interface ItemClient {
+    @GetMapping("/items")
+    List<ItemDTO> queryItemByIds(@RequestParam("ids")Collection<Long> ids);
+}
