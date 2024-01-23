2a48bc5c2a9f54ff9175dba31fbf2504c26f8cf5
dy <dyu32345@gmail.com>
2024-01-23 21:09:33 +0800

    黑马商城之支付模块拆分

---
+++ hm-api/src/main/java/com/hmall/api/client/TradeClient.java
@@ -0,0 +1,16 @@
+package com.hmall.api.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+
+/**
+ * @Author: dy
+ * @Date: 2024/1/23 20:23
+ * @Description:
+ */
+@FeignClient("trade-service")
+public interface TradeClient {
+    @PutMapping("orders/{orderId}")
+    void markOrderPaySuccess(@PathVariable("orderId") Long orderId);
+}
