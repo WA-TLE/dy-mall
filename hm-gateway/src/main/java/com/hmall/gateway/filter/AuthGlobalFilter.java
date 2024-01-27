2829ed56bdb99e6ccac79090803b0dd6b7ba88b7
dy <dyu32345@gmail.com>
2024-01-27 22:36:02 +0800

    定义全局过滤器, 获取当前登录用户信息

---
+++ hm-gateway/src/main/java/com/hmall/gateway/filter/AuthGlobalFilter.java
@@ -0,0 +1,87 @@
+package com.hmall.gateway.filter;
+
+import com.hmall.gateway.config.AuthProperties;
+import com.hmall.gateway.utils.JwtTool;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+/**
+ * @Author: dy
+ * @Date: 2024/1/27 18:18
+ * @Description:
+ */
+@Component
+@RequiredArgsConstructor
+public class AuthGlobalFilter implements GlobalFilter, Ordered {
+
+    private final JwtTool jwtTool;
+    private final AntPathMatcher antPathMatcher = new AntPathMatcher();
+    private final AuthProperties authProperties;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        //  1. 获取请求
+        ServerHttpRequest request = exchange.getRequest();
+
+        //  2. 判断是否需要拦截
+        if (isExclude(request.getPath().toString())) {
+            return chain.filter(exchange);
+        }
+
+        //  3.1 获取 token
+        List<String> headers = request.getHeaders().get("authorization");
+
+        //  3.2 可能未登录, 无法获得 token
+        String token = null;
+        if (headers != null && !headers.isEmpty()) {
+            token = headers.get(0);
+        }
+
+
+        //  4. 校验并解析 token  这里可能出现异常
+        //  Long userId = jwtTool.parseToken(token);
+        Long userId = null;
+
+        try {
+            userId = jwtTool.parseToken(token);
+        } catch (Exception e) {
+            //  设置相应状态码
+            ServerHttpResponse response = exchange.getResponse();
+            response.setStatusCode(HttpStatus.UNAUTHORIZED);
+            return response.setComplete();
+        }
+
+        //  5. 拿到用户信息, 保存
+        // TODO: 2024/1/27 下次做
+        System.out.println("userId = " + userId);
+
+        //  6. 放行
+        return chain.filter(exchange);
+
+    }
+
+    private boolean isExclude(String path) {
+        for (String excludePath : authProperties.getExcludePaths()) {
+            if (antPathMatcher.match(excludePath, path)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
